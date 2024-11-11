aed787d0de0bf14ef82063ee9acffa370c5d950c
Valentina Valdivia <val3.valdivia.g@gmail.com>
2024-11-11 15:04:26 -0300

    limpieza

---
+++ limpieza.py
@@ -0,0 +1,15 @@
+import funciones
+import os
+
+path_excel_original = os.path.join('datos_originales.xlsx')
+
+df = funciones.abrir_excel(path_excel_original)
+df_filtrado = funciones.eliminar_columnas(df)
+
+#guardado en excel
+
+funciones.guardar_en_excel(df_filtrado, 'datos_filtrados.xlsx')
+
+#guardado en csv
+
+funciones.guardar_en_csv(df_filtrado, 'datos_filtrados.csv')
